f6a8c8f8e79d931276c1665c0fee51513f7eb855
DevRaz02 <raulnascimento2002@gmail.com>
2026-07-28 08:47:34 -0300

    feat: projeto inicial limpo

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/PATINHAS-STORE/",
+  base: "/Patinhas-Shop/",
 });
